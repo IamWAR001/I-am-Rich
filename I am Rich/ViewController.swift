4821ddafce37fcbbfb8b3707c405d8d2e7d871ce
Walter Ramirez <ramirezwal@outlook.com>
2020-12-11 14:25:09 -0400

    I am Rich App

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Walter A Ramirez on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
